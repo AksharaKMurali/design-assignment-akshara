533187bb8c5e0365f419cf6a9c741e02fbe49617
AksharaKMurali <aksharakmurali@gmail.com>
2026-06-09 23:24:08 +0530

    Update fulladder_top.v

--- day1/ripple_carrry_adder/fulladder_top.v
+++ day1/ripple_carrry_adder/fulladder_top.v
@@ -1,1 +1,12 @@
 
+module fulladd(input a,b,c ,output sum, carry
+
+    );
+    wire w1,w2, w3;
+    xor(sum,a,b,c);
+    and(w1,a,b);
+    and(w2,a,c);
+    and(w3,b,c);
+    or(carry,w1,w2,w3);
+    
+endmodule
