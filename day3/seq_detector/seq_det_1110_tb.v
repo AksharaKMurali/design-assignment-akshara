a9f859ed3e30bcc557208b84cf2181686443f211
AksharaKMurali <aksharakmurali@gmail.com>
2026-06-10 17:46:44 +0530

    Update seq_det_1110_tb.v

--- day3/seq_detector/seq_det_1110_tb.v
+++ day3/seq_detector/seq_det_1110_tb.v
@@ -1,1 +1,46 @@
+module seq_detect_1110_tb(
+);
 
+reg clk_tb;
+reg rst_tb;
+reg din_tb;
+wire detected_tb;
+
+seq_det_1110 dut(
+    .clk(clk_tb),
+    .rst(rst_tb),
+    .din(din_tb),
+    .detected(detected_tb)
+);
+initial
+begin
+    clk_tb = 0;
+end
+
+always #5 clk_tb = ~clk_tb;
+
+initial
+begin
+    rst_tb = 1;
+    din_tb = 0;
+
+    #10;
+    rst_tb = 0;
+
+    // Sequence: 1110
+    #10 din_tb = 1;
+    #10 din_tb = 1;
+    #10 din_tb = 1;
+    #10 din_tb = 0;
+
+    #20;
+    $finish;
+end
+
+initial
+begin
+    $monitor("Time=%0t clk=%b rst=%b din=%b detected=%b",
+              $time, clk_tb, rst_tb, din_tb, detected_tb);
+end
+
+endmodule
