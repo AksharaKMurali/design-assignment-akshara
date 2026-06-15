01706dca931427fa9ce831ce519f6b33a9315a89
AksharaKMurali <aksharakmurali@gmail.com>
2026-06-15 21:49:11 +0530

    Update transaction_class_fifo.v

--- day6/transaction_class/transaction_class_fifo.v
+++ day6/transaction_class/transaction_class_fifo.v
@@ -1,28 +1,18 @@
 
 class transaction;
-
 rand bit rst_tb,wrenb_tb,rdenb_tb;
 rand bit [7:0] data_in_tb;
-
 bit [7:0] data_out_tb;
 bit full,empty;
-
 constraint c1 {
     rst_tb   dist {0:=8,1:=2};
     wrenb_tb dist {0:=2,1:=8};
     rdenb_tb dist {0:=8,1:=2};
-
     data_in_tb dist {
         8'hFF := 10,
         8'hAA := 5,
-        8'h55 := 5
-    };
+        8'h55 := 5};
 }
-
-constraint c2 {
-    !(wrenb_tb && rdenb_tb);
-}
-
 function void display();
     $display("rst=%0b wr=%0b rd=%0b din=%0h dout=%0h full=%0b empty=%0b",
              rst_tb,wrenb_tb,rdenb_tb,
@@ -30,4 +20,4 @@ function void display();
              full,empty);
 endfunction
 
-endclass
+ndclass
