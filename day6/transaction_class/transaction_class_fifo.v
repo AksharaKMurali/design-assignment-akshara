dea52b251ce9d07165ae0b84a717015c48fda2d5
AksharaKMurali <aksharakmurali@gmail.com>
2026-06-15 21:18:49 +0530

    Update transaction_class_fifo.v

--- day6/transaction_class/transaction_class_fifo.v
+++ day6/transaction_class/transaction_class_fifo.v
@@ -1,1 +1,16 @@
 
+class transaction;
+    rand bit  wr_enb;
+    rand bit  rd_enb;
+    rand bit [7:0]  data_in;
+
+    bit [7:0]  data_out;
+    bit full;
+    bit empty;
+
+  constraint c1 { !(wr_enb && rd_enb);}
+    function void display(string s);
+        $display("[%s] wr_enb=%0b rd_enb=%0b data_in=%0h data_out=%0h full=%0b empty=%0b",
+                 s, wr_enb, rd_enb, data_in, data_out, full, empty);
+    endfunction
+endclass
